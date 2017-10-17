e552b766a7956bec7058a5262327e1192b1b2cdb
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-10-17 22:39:09 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ms_lab2;
 
 import java.io.*;
 import static java.lang.System.err;
 
-/**
- *
- * @author Asus
- */
-public class main {
+public class Main {
 
     /**
      * @param args the command line arguments
@@ -21,14 +11,14 @@ public class main {
         // TODO code application logic here
         try{
             MiddleSquare ms= new MiddleSquare(8478);
+            LinearCongruentialGenerator lcg =new LinearCongruentialGenerator(8478,2,3,200);
             PrintWriter out = new PrintWriter( "output.txt" );
-            for(int i=1;i<=10000;i++)
-                out.println(ms.next());
+            for(int i=1;i<100;i++)
+                out.println(lcg.next());
             out.close();
         }catch (Exception e)
         {
             System.out.println("Got an exception! " + err);
         }
     }
-    
 }
